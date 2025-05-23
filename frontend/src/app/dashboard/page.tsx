3f758ac385b951ebe8cd4efafe243a3f0d6a5a54
Davi Lucciola <davilucciola14@gmail.com>
2025-05-23 10:20:21 -0300

    [refactor:frontend/deposit-form] - implementing depoist form

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -2,18 +2,6 @@ import { Menu } from "./components/menu";
 import { Header } from "./components/header";
 import { Content } from "./components/content";
 
-// import { useSession } from "next-auth/react";
-// import { Token } from "@/lib/token";
-// import { getServerSession } from "next-auth";
-// import { nextAuthOptions } from "@/lib/auth";
-// import { BalanceCard } from "@/modules/account/components/balance-card";
-// import { BankStatmentCard } from "@/modules/transaction/components/bank-statment-card";
-// import { AccountResumeCard } from "@/modules/transaction/components/transaction-resume-card";
-// import { AccountContext } from "@/modules/account/contexts/account-context";
-// import { useUnauthorizedHandler } from "@/modules/auth/hooks/use-unauthorized-handler";
-// import { MyAccount } from "@/modules/account/components/my-account";
-// import { Account } from "@/modules/account/schemas/account";
-
 export default function DashboardPage() {
   return (
     <div className="flex flex-row w-screen min-h-screen">
